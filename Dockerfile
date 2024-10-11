4945e2d70c25413f99333a5010cbdcb9030ff010
Jean-Baptiste LUCAS <jbaptlucas@gmail.com>
2024-10-11 15:31:22 +0200

    Adding Docker configuration and .env support for web app URL

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# Utilisation de l'image eclipse-temurin:21-jre-alpine comme base
+FROM eclipse-temurin:21-jdk-alpine
+
+# Installation de git et Maven
+RUN apk add --no-cache git maven
+
+# Définir le répertoire de travail dans le conteneur
+WORKDIR /app
+
+# Copy the pom.xml file
+COPY pom.xml .
+
+# Copier le fichier .env
+COPY .env .
+
+# Copy your source code
+COPY src /app/src
+
+# Construire le projet avec Maven
+RUN mvn package -DskipTests
+
+# Exposer le port sur lequel l'application s'exécutera
+EXPOSE 8085
+
+CMD export WEB_URL = $WEB_URL && source .env && mvn clean install exec:java
--- src/main/java/com/fitfeed/feed/WebConfig.java
+++ src/main/java/com/fitfeed/feed/WebConfig.java
@@ -1,5 +1,6 @@
 package com.fitfeed.feed;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+	private final Dotenv dotenv = Dotenv.load();
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:4200")
+			.allowedOrigins(dotenv.get("WEB_URL"))
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
 			.allowCredentials(true);
